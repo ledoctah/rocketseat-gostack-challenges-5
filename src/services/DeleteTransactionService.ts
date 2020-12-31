67f8f9f5f9d6c20c69757e25a35a03a3fa3c5fd1
ledoctah <matheusferr2015@gmail.com>
2020-12-30 21:26:51 -0300

    Added Delete Service function

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,25 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+import AppError from '../errors/AppError';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const found = await transactionRepository.findOne(id);
+
+    if (!found) {
+      throw new AppError(
+        `Transaction with id ${id} does not exists and cannot be deleted.`,
+      );
+    }
+
+    transactionRepository.delete(id);
   }
 }
 
